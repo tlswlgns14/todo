de0db239aa0e3e4c5c0c8aee658953cd741790bf
tlswlgns14 <tlswlgns14@naver.com>
2024-07-29 00:59:29 +0900

    d

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useDarkMode } from '../../context/DarkModeContext';
-import styles from './Header.module.css';
-import { HiMoon, HiSun } from 'react-icons/hi';
+import React from "react";
+import { useDarkMode } from "../../context/DarkModeContext";
+import styles from "./Header.module.css";
+import { HiMoon, HiSun } from "react-icons/hi";
 
 export default function Header({ filters, filter, onFilterChange }) {
   const { darkMode, toggleDarkMode } = useDarkMode();
