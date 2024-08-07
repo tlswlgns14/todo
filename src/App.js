5387d664afd7dde5c4486a633569ede6eb02ee63
tlswlgns14 <tlswlgns14@naver.com>
2024-08-08 02:26:49 +0900

    D

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Videos from "./pages/Videos";
-import Root from "./pages/Root";
-import VideoDetail from "./pages/VideoDetail";
-
-// import TodoList from "./components/TodoList/TodoList";
+import TodoList from "./components/TodoList/TodoList";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/videos", element: <Videos /> },
-      { path: "/videos/:videoId", element: <VideoDetail /> },
-    ],
-  },
-]);
 const filters = ["all", "active", "completed"];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <RouterProvider router={router} />
-    // <DarkModeProvider>
-    //   <Header filters={filters} filter={filter} onFilterChange={setFilter} />
-    //   {/* <TodoList filter={filter} /> */}
-    // </DarkModeProvider>
+    <DarkModeProvider>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </DarkModeProvider>
   );
 }
 
--- src/pages/VideoDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export default function VideoDetail() {
-  const { videoId } = useParams();
-  console.log(videoId);
-
-  return <div>videoDetail {videoId}</div>;
-}
